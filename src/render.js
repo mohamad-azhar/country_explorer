09447b13b4769a1e6b252baf50c389b57d2624f6
mohamad-azhar <mohamad.sheikh.aleshra@student.ehb.be>
2026-05-07 18:26:01 +0200

    country card function updated

--- src/render.js
+++ src/render.js
@@ -3,23 +3,31 @@
 // Haalt de container op waar de kaartjes in komen
 const grid = document.getElementById('countries_grid');
 
-// Maakt 1 kaartje voor 1 land
-const creatCard = (country) => {
-    const name = country.name.common;
-    const flag = country.flags.svg;
-    const capital = country.capital?.[0] ?? 'Onbekend';
-    const population = country.population.toLocaleString('nl-BE');
-    const area = country.area ? country.area.toLocaleString('nl-BE') + ' km²' : 'Onbekend';
-    const region = country.region;
-    const subregion = country.subregion ?? '—';
 
-    // Talen samenvoegen tot een string
-    const languages = country.languages
-    ? Object.values(country.languages).join(', ')
-    : 'Onbekend';
+// Class die één landenkaartje voorstelt
+class CountryCard {
+    
+    constructor (country){
+        this.name = country.name.common;
+        this.flag = country.flags.svg;
+        this.capital = country.capital?.[0] ?? 'Onbekend';
+        this.population = country.population.toLocaleString('nl-BE');
+        this.area = country.area ? country.area.toLocaleString('nl-BE') + ' km²' : 'Onbekend';
+        this.region = country.region;
+        this.subregion = country.subregion ?? '—';
+        this.cca3 = country.cca3
 
-    //Munteenheid ophalen
-    this.currency = country.currencies
-    ? Object.values(countries_currencies).map(c => `${c.name} (${c.symbol ?? '?'})`).join(', ')
-    : 'Onbekend';
+        // Talen samenvoegen tot een string
+        const languages = country.languages
+            ? Object.values(country.languages).join(', ')
+            : 'Onbekend';
+
+        //Munteenheid ophalen
+        this.currency = country.currencies
+            ? Object.values(countries_currencies).map(c => `${c.name} (${c.symbol ?? '?'})`).join(', ')
+            : 'Onbekend';
+    }
+
 }
+
+
