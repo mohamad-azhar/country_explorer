50b26ec7331f0df61c446daefaa8a138fb6d0e35
mohamad-azhar <mohamad.sheikh.aleshra@student.ehb.be>
2026-05-09 14:41:33 +0200

    add toggle favourite and export

--- src/favourite.js
+++ src/favourite.js
@@ -5,15 +5,32 @@
 const getFavourites = () => {
     try {
         const stored = localStorage.getItem(STORAGE_KEY);
-        return stored ? JSON.parse(stored): [];
+        return stored ? JSON.parse(stored) : [];
     } catch (error) {
         console.error(`Fout bij het ophalen van favorieten: ${error.message}`);
-        return [];   
+        return [];
     }
 
 }
 
 // Checken of een land favoriet is
-const isFavourite = (name) =>{
+const isFavourite = (name) => {
     return getFavourites().includes(name);
 }
+
+// Favoriet toevoegen of verwijderen (toggle)
+const toggleFavourite = (name) => {
+    const favourites = getFavourites();
+    if (favourites.includes(name)) {
+        // Verwijderen uit favorieten
+        const updated = favourites.filter(fav => fav !== name);
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(updated));
+    } else {
+        // Toevoegen aan favorieten
+        favourites.push(name);
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(favourites));
+    }
+}
+
+
+export {getFavourites, isFavourite, toggleFavourite}
