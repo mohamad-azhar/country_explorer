942642c789a8ab06a2e75573dcd46396c66538ce
mohamad-azhar <mohamad.sheikh.aleshra@student.ehb.be>
2026-05-11 20:49:38 +0200

    add main.js with imports and DOM element selectors

--- src/main.js
+++ src/main.js
@@ -1,11 +1,24 @@
 'use strict'
 import { fetchCountries } from './api.js' // Haalt de functie fetchCountries binnen uit api.js (data ophalen)
 import { renderCountries } from './render.js' // Haalt de functie renderCountries binnen uit render.js (weergave op scherm)
+import { applyFilters } from './filter.js' 
+import { getFavourites, toggleFavourite } from './favourite.js' 
+import { initTheme } from './theme.js' 
 
 
 // Foutmelding element
 const errorMsg = document.getElementById('error_msg');
 // Zoekt in de HTML naar het element met id "error-msg" om fouten te tonen
+
+const searchInput = document.getElementById('search');
+const regionSelect = document.getElementById('filter_region');
+const sortSelect = document.getElementById('sort');
+const showFavBtn = document.getElementById('show_favourites');
+const grid = document.getElementById('countries_grid');
+
+// Globale variabele om alle landen bij te houden
+let allCountries = [];
+let showingFavourites = false;
 
 // App initialiseren
 const init = async ()=> {
